5611a34f7896cc36b5d082c2fefe02aa4af662b0
Arkid Kaleci <kaleciarkid@gmail.com>
2022-11-06 00:34:43 +0100

    feat: Add passwords, testing

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct User {
     pub id: u32,
     pub name: String,
@@ -5,21 +6,19 @@ pub struct User {
     pub password: String,
     pub inventory: Inventory,
     pub achievements: Achievements,
-    pub daily: DailyQuest,
+    pub daily_quests: DailyQuest,
     pub stats: Stats,
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Inventory {
     pub coins: u32,
-    pub trees: Trees,
-}
-
-pub struct Trees {
     pub oak: u32,
     pub beech: u32,
     pub maple: u32,
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Achievements {
     pub oak_planted: bool,
     pub beech_planted: bool,
@@ -35,18 +34,87 @@ pub struct Achievements {
     pub nature_lover: bool, // nature_lover: achieved when you are a beginner
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct DailyQuest {
     pub general: bool,
     pub specific: bool,
     pub wholesome: bool,
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Stats {
     pub days_logged: u32,
     pub days_logged_streak: u32,
     pub trees_planted: u32,
     pub trash_cleaned: u32,
     pub coins_collected: u32,
+}
+
+impl Default for User {
+    fn default() -> Self {
+        Self {
+            id: 0,
+            name: String::new(),
+            email: String::new(),
+            password: String::new(),
+            inventory: Inventory::default(),
+            achievements: Achievements::default(),
+            daily_quests: DailyQuest::default(),
+            stats: Stats::default(),
+        }
+    }
+}
+
+impl Default for Inventory {
+    fn default() -> Self {
+        Self {
+            coins: 0,
+            oak: 0,
+            beech: 0,
+            maple: 0,
+        }
+    }
+}
+
+impl Default for Achievements {
+    fn default() -> Self {
+        Self {
+            oak_planted: false,
+            beech_planted: false,
+            maple_planted: false,
+            days_logged_in: 0,
+            days_logged_streaks: 0,
+            trees_planted: 0,
+            trash_cleaned: 0,
+            coins_collected: 0,
+            plastic_hero: 0,
+            science_wizz: 0,
+            tree_hugger: 0,
+            nature_lover: false,
+        }
+    }
+}
+
+impl Default for DailyQuest {
+    fn default() -> Self {
+        Self {
+            general: false,
+            specific: false,
+            wholesome: false,
+        }
+    }
+}
+
+impl Default for Stats {
+    fn default() -> Self {
+        Self {
+            days_logged: 0,
+            days_logged_streak: 0,
+            trees_planted: 0,
+            trash_cleaned: 0,
+            coins_collected: 0,
+        }
+    }
 }
 
 pub enum Trash {
