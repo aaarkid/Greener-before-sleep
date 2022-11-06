bd5c1e785286af301d6b83b4ca33c970bda9040d
Arkid Kaleci <kaleciarkid@gmail.com>
2022-11-06 10:56:29 +0100

    feat: Finish backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -8,6 +8,7 @@ use testing::*;
 use common::*;
 use std::{collections::HashMap, sync::{atomic::AtomicU32, Arc}};
 use warp::Filter;
+use reqwest::Client;
 
 #[tokio::main]
 async fn main() {
@@ -19,24 +20,34 @@ async fn main() {
     let quotes = std::str::from_utf8(&contents).unwrap().lines().map(|s| s.to_string()).collect::<Vec<String>>();
     
     //create a get route that gives the current day
-    let get_date = warp::path!("date").map(|| day.load(std::sync::atomic::Ordering::Relaxed));
+    let get_date = warp::path!("date").map(|| {
+        warp::reply::json(&day.load(std::sync::atomic::Ordering::Relaxed))
+    });
 
     let get_quote = warp::path!("quote").map(|| {
-        let day = day.load(std::sync::atomic::Ordering::Relaxed);
-        let quote = quotes[day as usize - 1].clone();
-        quote
+        warp::reply::json(&day.load(std::sync::atomic::Ordering::Relaxed))
     });
 
     
     //create a post route that increments the day and calls a function
     let post_date = warp::path!("add")
         .and(warp::post())
-        .map(move || {
+        .map(|| {
+            //make a post request to the link https://api.twilio.com/2010-04-01/Accounts/AC2aca0b0375acdff08df264a951dc993e/Messages.json with data
+            let client = Client::new();
+            let _ = client.post("https://api.twilio.com/2010-04-01/Accounts/AC2aca0b0375acdff08df264a951dc993e/Messages.json")
+                .basic_auth("AC2aca0b0375acdff08df264a951dc993e", Some("8f169004f8dc6c39df637578c575f117"))
+                .form(&[("To", "+355699751977"), ("MessagingServiceSid", "MGe662c305172f061ad61d99e309a9cb64"), ("Body", "Don't forget to log in GBS today and feel good about your effort to make our planet greener! www.sustaining.tech/greener")])
+                .send();
+
             if (day.load(std::sync::atomic::Ordering::Relaxed) < 19) {
-                day.fetch_add(1, std::sync::atomic::Ordering::Relaxed)
+                warp::reply::json(&day.fetch_add(1, std::sync::atomic::Ordering::Relaxed))
             } else {
                 day.store(1, std::sync::atomic::Ordering::Relaxed);
-                day.load(std::sync::atomic::Ordering::Relaxed)
+                warp::reply::json(&day.load(std::sync::atomic::Ordering::Relaxed))
             }
         });
+
+    //serve the routes
+    warp::serve(get_date).run(([127, 0, 0, 1], 3030)).await;
 }
