8116f1e430cb38de26d87467f3a16120b0f56a4b
Arkid Kaleci <kaleciarkid@gmail.com>
2022-11-06 05:46:40 +0100

    chore: Fix css

--- frontend/src/components/dashboard.rs
+++ frontend/src/components/dashboard.rs
@@ -9,17 +9,16 @@ pub fn dashboard() -> Html {
 <>
     <section>
         <div class="navbar">
-            <ul style="margin-top: 0px">
+            <ul style="margin-top: 0px; margin-bottom: 0px;">
                 //number of icons and a coin icon
                 <img src="images/logo.png" alt="" style="padding-left: 5px; padding-top: 5px; height: 40px;"/>
                 <li><Link<Route> to={Route::Home}>{"Logout"}</Link<Route>></li>
                 <li style="
                 color: white;
                 text-align: center;
-                padding: 14px 16px;
                 text-decoration: none;
-                "><img src="images/icons/coin.png" alt="coin" style="width: 20px; height: 20px; padding-right
-                "/>{"80 Coins"}</li>
+                padding: 14px 16px;
+                "><img src="images/icons/coin.png" alt="coin" style="width: 15px; height: 15px;"/>{"80 Coins"}</li>
             </ul>
         </div>
     </section>
@@ -29,10 +28,10 @@ pub fn dashboard() -> Html {
         <div class="pane">
             <ul>
                 //each button has a big icon a title under
-                <li><img src="images/collect.png" alt="collect" style="width: 50; height: 50; margin-top: 5px;"/>{"Collect"}</li>
-                <li><img src="images/daily.png" alt="daily" style="width: 50; height: 50; margin-top: 5px;"/>{"Daily Tasks"}</li>
-                <li><img src="images/achievments.png" alt="achievments" style="width: 50; height: 50; margin-top: 5px;"/>{"Achievments"}</li>
-                <li><img src="images/plant.png" alt="plant" style="width: 50; height: 50; margin-top: 5px;"/>{"Plant"}</li>
+                <li><img src="images/icons/collect.png" alt="collect" style="width: 50px; height: 50px; margin-top: 5px;"/>{"Collect"}</li>
+                <li><img src="images/icons/daily.png" alt="daily" style="width: 50px; height: 50px; margin-top: 5px;"/>{"Daily Tasks"}</li>
+                <li><img src="images/icons/achievments.png" alt="achievments" style="width: 50px; height: 50px; margin-top: 5px;"/>{"Achievments"}</li>
+                <li><img src="images/icons/quests.png" alt="plant" style="width: 50px; height: 50px; margin-top: 5px;"/>{"Plant"}</li>
             </ul>
         </div>
     </section>
