08bc5eaef4360ca14a4cd26447dd0ba5a443889c
Arkid Kaleci <kaleciarkid@gmail.com>
2022-11-06 01:00:09 +0100

    chore: Move frontend files

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -14,7 +14,11 @@ async fn main() {
     let mut users: Vec<User> = Vec::new();
     users.push(create_example_user_1());
 
-    //create a post route that receives a user and adds it to the users vector
+    //create a route that receives a name, email, and plain password and adds it to the users vector
+    warp::serve(warp::post()
+        .and(warp::path("register"))
+        .and(warp::body::json())
+        .and_then(register));
     
     warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
 }
